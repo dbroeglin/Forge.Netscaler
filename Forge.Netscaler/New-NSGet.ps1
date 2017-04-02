f85ce5a47640be3a97cf1f00dd9ff781cc1f6c8e
Dominique Broeglin <dominique.broeglin@gmail.com>
2017-04-02 11:21:37 +0200

    Refactored New-NSGet and added partial and resourceid param name

--- Forge.Netscaler/New-NSGet.ps1
+++ Forge.Netscaler/New-NSGet.ps1
@@ -1,10 +1,10 @@
 function New-NSGet {
     <#
     .SYNOPSIS
-        TODO.
+        Creates a new "Get" function for the Netscaler Module.
 
     .DESCRIPTION
-        TODO: description.
+        Creates a new "Get" function for the Netscaler Module.
 
     .EXAMPLE
         New-NSGet -Name MyName
@@ -14,8 +14,15 @@ function New-NSGet {
     .PARAMETER Name
         Name of the entity for which the function is generated.
 
+    .PARAMETER ResourceIdParamName
+        Name of the paramter that will be used to identify a Nitro resource by
+        its identifier.
+
+        Default value: Name
+
     .PARAMETER Path
-        Path where the function should be generated.
+        Path where the function should be generated. By default it will be generated
+        in ./Netscaler/Public/
 
     .PARAMETER Type
         Type of netscaler object the function retrieves.
@@ -23,11 +30,15 @@ function New-NSGet {
     .PARAMETER Label
         Label to use in the function documentation.
 
-    .PARAMETER FilterList
+    .PARAMETER Filters
         Array of filters the function will allow. 
 
     .PARAMETER Author
         Author of this function
+
+    .PARAMETER Partial
+        If true, a note is added to the comment-base help topic to indicate thate the
+        implemenation is only partial.
 
     #>
     [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact='Low')]
@@ -35,24 +46,44 @@ function New-NSGet {
         [Parameter(Mandatory = $true)]
         [String]$Name,
 
-        [String]$Path = ".\",
+        [String]$ResourceIdParamName = "Name",
+
+        [String]$Path = "Netscaler/Public/",
 
         [String]$Type = $Name.ToLower(),
     
         [Parameter(Mandatory = $true)]
         [String]$Label,
 
-        [Hashtable]$FilterList = @{},  
+        [Hashtable]$Filters = @{},  
 
         [Switch]$Arguments,
 
-        [String]$Author
+        [String]$Author,
+
+        [Switch]$Partial
     )
+
     Begin {
         Initialize-ForgeContext -SourceRoot (Join-Path $PSScriptRoot "Templates") `
             -DestinationPath $Path
     }
     Process {
+        function PrepareFilters {
+            Param($Filters)
+            $Result = [ordered]@{}
+
+            $Filters.GetEnumerator() | ForEach-Object {
+                $FilterName, $FilterDesc = $_.Key, $_.Value
+
+                if ($FilterDesc -is [String]) {
+                    $FilterDesc = @($FilterDesc, $FilterDesc)
+                } 
+
+                $Result[$FilterName] = $FilterDesc
+            }
+            $Result
+        }
         if (!$PSCmdlet.ShouldProcess($Name, "Create New Netscaler Get")) {
             return
         }
@@ -61,14 +92,16 @@ function New-NSGet {
         $CopyrightYear = Get-Date -UFormat %Y
 
         Set-ForgeBinding @{
-            Name          = $Name
-            FunctionName  = $FunctionName
-            Type          = $Type
-            Label         = $Label
-            FilterList    = $FilterList
-            Arguments     = $Arguments
-            Author        = $Author
-            CopyrightYear = $CopyrightYear
+            Name                = $Name
+            ResourceIdParamName = $ResourceIdParamName
+            FunctionName        = $FunctionName
+            Type                = $Type
+            Label               = $Label
+            Filters             = PrepareFilters $Filters
+            Arguments           = $Arguments
+            Author              = $Author
+            CopyrightYear       = $CopyrightYear
+            Partial             = $Partial
         }
 
         Copy-ForgeFile -Source "NSGet" -Destination "$FunctionName.ps1"
